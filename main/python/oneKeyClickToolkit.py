584096bc48096a816b07102bd1c8cfb1f67361c1
codervibe <3507557934@qq.com>
2024-06-18 22:04:32 +0800

    更新更多功能

--- main/python/oneKeyClickToolkit.py
+++ main/python/oneKeyClickToolkit.py
@@ -1,6 +1,164 @@
+from util.simulateprogressbar import simulate_progress_bar
 from onekeyclickfucksatellite import onekeyclickfucksatellite
 from onekeyclickfuckserver import informationcollection
 
+
+
+
+
+def 一键顺着网线砍人():
+    try:
+        print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键顺着网线砍人", 80)
+        print("一键顺着网线砍人完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键给对手戴绿帽():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键给对手戴绿帽", 80)
+        print("你已经送给对手一顶 大绿帽\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键召唤陨石砸向对手():
+    try:
+        print("正在执行一键召唤陨石砸向对手...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在念动咒语", 80)
+        simulate_progress_bar("召唤陨石砸向对手", 60)
+        print("一一键召唤陨石砸向对手完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键拔掉对方网线():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键拔掉对方网线", 80)
+        print("对方网线已经被切断\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键盗QQ号():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在给对方传播木马", 80)
+        simulate_progress_bar("正在等待对方上钩", 60)
+        simulate_progress_bar("对方已上钩 正在盗取", 60)
+        print("qq号现在是我们的了 任务完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键日进内网():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在搜索对方内网漏洞", 90)
+        simulate_progress_bar("已找到对方漏洞正在尝试利用", 60)
+        simulate_progress_bar("正在释放病毒", 60)
+        simulate_progress_bar("正在提权", 60)
+        print("成功 现在对方内网是我们的了!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键挖洞():
+    try:
+        # print("正在执行一键挖洞...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在寻找漏洞", 60)
+        print("任务完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键刷Q币():
+    try:
+        # print("正在执行一键刷Q币...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键刷Q币", 160)
+        print("完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键挖比特币():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键挖比特币", 160)
+        print("一键顺着网线砍人完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键刷会员():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("一键刷会员", 160)
+        print("一键顺着网线砍人完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键日教务系统():
+    try:
+        print("正在执行一键日教务系统...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在查找教务系统漏洞", 60)
+        simulate_progress_bar("正在尝试弱密码爆破", 60)
+        print("密码已找到!\n")
+        simulate_progress_bar("正在尝试登录", 60)
+        print("登陆完成.......!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键社工管理员全家():
+    try:
+        print("正在执行一键社工管理员全家")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在查找管理员个人信息", 260)
+        simulate_progress_bar("正在查找管理员全家信息", 160)
+        print("信息已找到 任务完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
+
+
+def 一键DDOS():
+    try:
+        # print("正在执行一键顺着网线砍人...")
+        # simulate_progress_bar("一键日服务器", 89)
+        simulate_progress_bar("正在发动DDOS 攻击", 160)
+        simulate_progress_bar("正在模拟正常用户访问", 160)
+        print("对方服务器已经未响应!\n")
+        print("任务完成!\n")
+    except KeyboardInterrupt:
+        print("\n程序被中断.")
+        exit()
 
 def print_menu():
     """
@@ -9,9 +167,20 @@ def print_menu():
     print("\n请选择一个操作:")
     print("1. 一键日卫星")
     print("2. 一键日服务器")
+    print("3. 一键顺着网线砍人")
+    print("4. 一键给对手戴绿帽")
+    print("5. 一键召唤陨石砸向对手")
+    print("6. 一键拔掉对方网线")
+    print("7. 一键盗QQ号")
+    print("8. 一键日进内网")
+    print("9. 一键挖洞")
+    print("10. 一键刷Q币")
+    print("11. 一键挖比特币")
+    print("12. 一键刷会员")
+    print("13. 一键日教务系统")
+    print("14. 一键社工管理员全家")
+    print("15. 一键DDOS")
     print("0. 退出")
-
-
 def perform_action(choice):
     """
     根据用户选择执行相应的操作.
@@ -21,6 +190,32 @@ def perform_action(choice):
             perform_scan_and_select_satellite()
         elif choice == '2':
             perform_daily_server()
+        elif choice == '3':
+            一键顺着网线砍人()
+        elif choice == '4':
+            一键给对手戴绿帽()
+        elif choice == '5':
+            一键召唤陨石砸向对手()
+        elif choice == '6':
+            一键拔掉对方网线()
+        elif choice == '7':
+            一键盗QQ号()
+        elif choice == '8':
+            一键日进内网()
+        elif choice == '9':
+            一键挖洞()
+        elif choice == '10':
+            一键刷Q币()
+        elif choice == '11':
+            一键挖比特币()
+        elif choice == '12':
+            一键刷会员()
+        elif choice == '13':
+            一键日教务系统()
+        elif choice == '14':
+            一键社工管理员全家()
+        elif choice == '15':
+            一键DDOS()
         elif choice == '0':
             print("退出程序.")
             exit()
